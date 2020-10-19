9e3bd61d5291610678ec53ebcf2cf8b37765675f
Hannah Bannan <hannahbannan@MacBook-Pro-3.local>
2020-10-19 08:04:00 -0700

    Updates

---
+++ precache-manifest.a668d54d75e475ed52986457f8f67bb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5668a1b3febde419f5fbc455cdf25b95",
+    "url": "/Naya-Hackathon/index.html"
+  },
+  {
+    "revision": "acbe0bcbac5fd9b8eea5",
+    "url": "/Naya-Hackathon/static/css/main.86598365.chunk.css"
+  },
+  {
+    "revision": "910e3114eba69306ffe7",
+    "url": "/Naya-Hackathon/static/js/2.63065c24.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Naya-Hackathon/static/js/2.63065c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acbe0bcbac5fd9b8eea5",
+    "url": "/Naya-Hackathon/static/js/main.7559d334.chunk.js"
+  },
+  {
+    "revision": "0fcf37b05933c7081a87",
+    "url": "/Naya-Hackathon/static/js/runtime-main.8b1a1892.js"
+  }
+]);
